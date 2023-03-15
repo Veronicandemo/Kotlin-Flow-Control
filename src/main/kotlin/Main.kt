2f6375030fa12f89c3b6132ab2f996b76ada1f9c
Veronicandemo <ndemoveronica@gmail.com>
2023-03-15 13:18:23 +0300

    Kotlin Flow Control

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -24,8 +24,8 @@ fun arrNames(names: Array<String>): Int {
 //For making decisions depending on age
     fun guestAge(age: Any) {
         when (age) {
-            in 0..6 -> println("Milk")
-            in 7..14 -> println("Fanta Orange")
+            in 0..5 -> println("Milk")
+            in 6..14 -> println("Fanta Orange")
             else -> println("Bottle of Coca Cola")
         }
     }
