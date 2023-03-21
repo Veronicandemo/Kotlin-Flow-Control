8e4a743456aebd73595f31b83a1c561be7cce11a
Veronicandemo <ndemoveronica@gmail.com>
2023-03-21 17:47:31 +0300

    changed a few lines

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,35 +4,37 @@ fun main(){
     oddNums()
     fizzBuzz()
     guestAge(30)
+    guestAge(6)
+    guestAge(4)
     println(arrNames(arrayOf("Ndemo", "Veronica", "Hopper Lab", "AkiraChix", "Vee")))
     }
 //Create a function that prints out all the odd numbers between 1 and 100 (2pts)
-fun oddNums() :Int{
-    var numbers = 0
+fun oddNums(){
     for (num in 1..100) {
         if (num % 2 != 0) {
             println(num)
 //            println(numbers{.Random.nextInt(0, 100) })
         }
     }
-    return numbers
+
 }
 
-//Takes in arrays of names and retirns the number of names longer than 5 chars
+//Takes in arrays of names and returns the number of names longer than 5 chars
 fun arrNames(names: Array<String>): Int {
     var number = 0
     for (name in names) {
         if (name.length > 5) {
-            number++
+            println(name)
+           number ++
         }
     }
-        return number
+    return number
 }
 //For making decisions depending on age
-    fun guestAge(age: Any) {
+    fun guestAge(age: Int) {
         when (age) {
             in 0..5 -> println("Milk")
-            in 6..14 -> println("Fanta Orange")
+            in 7..14 -> println("Fanta Orange")
             else -> println("Bottle of Coca Cola")
         }
     }
@@ -41,7 +43,7 @@ fun arrNames(names: Array<String>): Int {
 //for multiples of both 3 and 5.
     fun fizzBuzz() {
         for (num in 1..100) {
-            if(num % 3 == 0 && num % 5 == 0){
+            if(num % 15 === 0){
                 println("FizzBuzz")
             }
             else if (num % 3 == 0) {
